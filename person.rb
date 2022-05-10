285c4a503e6e1fe744b5c19a3c8b91b3a848a34d
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-10 07:24:23 +0100

    Add is_of_age? or can_use_services? methods

--- person.rb
+++ person.rb
@@ -2,12 +2,21 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, parent_permission='true', name='unknown')
+  def initialize(age, parent_permission = 'true', name = 'unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  private
+
+  def is_of_age?
+    age >= 18
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permission == true
+  end
 end
 
