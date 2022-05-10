365868c4160ca7f960e0118bf23917be851cb363
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-10 08:41:37 +0100

    Refactor code

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, parent_permission = 'true', name = 'unknown')
+  def initialize(age, name = 'unknown', parent_permission = 'true')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
