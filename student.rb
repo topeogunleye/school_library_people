359b2f99cf8150eb82424afc0ef4486380e951d6
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-10 08:37:54 +0100

    Add arguments from the parent

--- student.rb
+++ student.rb
@@ -1,8 +1,11 @@
 # Student Class Inheriting From Person Class
 class Student < Person
-  def initialize(classroom)
-    super
+  def initialize(classroom, age, parent_permission = 'true', name = 'unknown')
+    super(name, age, parent_permission = 'true')
     @classroom = classroom
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
 
   def play_hooky
--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,11 @@
 # Teacher Class Inheriting From Person Class
 class Teacher < Person
   def initialize(specialization)
-    super
+    super(name, age, parent_permission = 'true')
     @specialization = specialization
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
