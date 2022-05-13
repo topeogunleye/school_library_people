7b6fd708f8a2de881813cbf1d9e116d01999ca0e
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-13 11:32:00 +0100

    Add student.rb

---
+++ student.rb
@@ -0,0 +1,24 @@
+require_relative './classroom'
+require_relative './person'
+
+# Student Class Inheriting From Person Class
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'unknown', parent_permission = 'true')
+    super(name, age, parent_permission)
+    @classroom = classroom
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
