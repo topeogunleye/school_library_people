2f963bf8c51a5d81ea2e3e1ca64999422a18f339
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-13 08:35:38 +0100

    modify the Book class in book.rb to include @rentals and  a getter for it:

--- book.rb
+++ book.rb
@@ -1,10 +1,13 @@
 # Book Class
 class Book
+  attr_reader :rentals
   attr_accessor :title, :author
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
   end
+
 
 end
