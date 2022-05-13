1a48595433418dcecbe9780ff490ca9139f3e127
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-13 10:47:22 +0100

    Fix linters

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ require_relative './book'
 maria = Person.new(45, 'Maria German')
 john = Person.new(58, 'John Doe')
 
-book_1 = Book.new('Jack Sparrow', 'James Bond')
-book_2 = Book.new('Evil Forest', 'Maria Carrie')
+BOOK1 = Book.new('Jack Sparrow', 'James Bond')
+BOOK2 = Book.new('Evil Forest', 'Maria Carrie')
 
-rental_1 = Rental.new('2017-12-22', john, book_1 )
-rental_2 = Rental.new('2017-12-31', maria, book_1)
+RENTAL1 = Rental.new('2017-12-22', john, BOOK1)
+RENTAL2 = Rental.new('2017-12-31', maria, BOOK2)
 
 puts john.correct_name
-puts book_1.author
-puts book_2.title
-puts book_1.rentals
+puts BOOK1.author
+puts BOOK2.title
+puts BOOK1.rentals
