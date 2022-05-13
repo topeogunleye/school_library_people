f95f3be45b0c99f8fb8cb8416bf84bb81eba88bd
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-13 11:23:58 +0100

    Revert "Revert "Feature/school library associations""

---
+++ book.rb
@@ -0,0 +1,11 @@
+# Book Class
+class Book
+  attr_reader :rentals
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+end
---
+++ rental.rb
@@ -0,0 +1,15 @@
+# Rental Class
+class Rental
+  attr_reader :person, :book
+  attr_accessor :date
+
+  def initialize(date, person, book)
+    @date = date
+
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
+  end
+end
