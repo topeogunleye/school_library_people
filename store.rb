12cf1f5591383aa8e60d18430567e8af8d733f60
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-23 18:23:34 +0100

    Convert Store class to module

--- store.rb
+++ store.rb
@@ -1,13 +1,9 @@
 require_relative './student'
 
-class Store
-  attr_reader :books_arr, :people_arr, :rentals_arr
-
-  def initialize
-    @books_arr = []
-    @people_arr = []
-    @rentals_arr = []
-  end
+module Store
+  @books_arr = []
+  @people_arr = []
+  @rentals_arr = []
 
   def book_store(book)
     @books_arr.push(book)
@@ -23,11 +19,18 @@ class Store
     @rentals_arr.push(rental)
     @rentals_arr
   end
+
+  def books_arr
+    @books_arr
+  end
+
+  def people_arr
+    @people_arr
+  end
+
+  def rentals_arr
+    @rentals_arr
+  end
 end
 
-store = Store.new
-student = Student.new(15, 'Daniel')
-
-store.people_store(student)
-@people_arr = store.people_arr
-@people_arr.each { |person| puts "[#{person.class}]: Name: #{person.name}, ID: #{person.id} AGE: #{person.age}" }
+include Store
