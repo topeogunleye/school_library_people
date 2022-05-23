a0a63cdd5530e542a93ed8dcc729a1f3b4b017c4
Islam Bahnas <islambahnas1@gmail.com>
2022-05-23 19:39:41 +0200

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,23 +1,42 @@
-require_relative './classroom'
-require_relative './person'
+require_relative './student'
 
-# Student Class Inheriting From Person Class
-class Student < Person
-  attr_reader :classroom
+class Store
+  attr_reader :books_arr, :people_arr, :rentals_arr
 
-  def initialize(age, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission: true)
-    @age = age
-    @name = name
-    @parent_permission = parent_permission
+  def initialize
+    @books_arr = []
+    @people_arr = []
+    @rentals_arr = []
   end
+module Store
+  @books_arr = []
+  @people_arr = []
+  @rentals_arr = []
 
-  def play_hooky
-    "¯\(ツ)/¯"
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+  def book_store(book)
+    @books_arr.push(book)
+@ -23,11 +19,18 @@ class Store
+    @rentals_arr.push(rental)
+    @rentals_arr
   end
 end
+
+store = Store.new
+student = Student.new(15, 'Daniel')
+  def books_arr
+    @books_arr
+  end
+
+  def people_arr
+    @people_arr
+  end
+
+  def rentals_arr
+    @rentals_arr
+  end
+end
+
+store.people_store(student)
+@people_arr = store.people_arr
+@people_arr.each { |person| puts "[#{person.class}]: Name: #{person.name}, ID: #{person.id} AGE: #{person.age}" }
+include Store
