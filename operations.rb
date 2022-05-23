97db0663bc87af86ad217421efc0c30b66df10b7
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-23 18:24:08 +0100

    Replace Store object with Store module

--- operations.rb
+++ operations.rb
@@ -1,18 +1,17 @@
 require_relative './store'
 
 class Operations
+  include Store
+
   def list_books
-    store = Store.new
-    books_arr = store.books_arr
-    books_arr.each do |book|
+    Store.books_arr.each do |book|
       puts "Title: #{book.title}, Author: #{book.author}"
     end
     run
   end
 
   def list_people
-    store = Store.new
-    store.people_arr.each do |person|
+    Store.people_arr.each do |person|
       puts "[#{person.class}]: Name: #{person.name}, ID: #{person.id} AGE: #{person.age}"
     end
     run
@@ -31,8 +30,7 @@ class Operations
     puts 'What is the teachers specialization?'
     subject = gets.chomp
     teacher = Teacher.new(subject, age, name)
-    store = Store.new
-    store.people_store(teacher)
+    Store.people_store(teacher)
     success('Teacher')
   end
 
@@ -56,8 +54,7 @@ class Operations
     name, age = user_info
     permission = choice_permission
     student = Student.new(age, name, parent_permission: permission)
-    store = Store.new
-    store.people_store(student)
+    Store.people_store(student)
     success('Student')
   end
 
@@ -80,41 +77,35 @@ class Operations
     puts 'Author: '
     author = gets.chomp
     book = Book.new(title, author)
-    store = Store.new
-    store.book_store(book)
+    Store.book_store(book)
     success('Book')
     run
   end
 
   def create_rental
     puts 'Select a book from the following list by number'
-    @books_arr.each_with_index { |book, index| puts "#{index} Title: #{book.title}, Author: #{book.author}" }
+    Store.books_arr.each_with_index { |book, index| puts "#{index} Title: #{book.title}, Author: #{book.author}" }
     book_index = gets.chomp.to_i
     puts 'Select a person from the following list by number (not id)'
-    @people_arr.each_with_index do |person, index|
+    Store.people_arr.each_with_index do |person, index|
       puts "#{index} [#{person.class}]: Name: #{person.name}, ID: #{person.id} AGE: #{person.age}"
     end
     person_index = gets.chomp.to_i
     print 'Enter the date [YYYY-MM-DD]: '
     date = gets.chomp
-    book = @books_arr[book_index]
-    person = @people_arr[person_index]
+    book = Store.books_arr[book_index]
+    person = Store.books_arr[person_index]
     rental = Rental.new(date, person, book)
-    store = Store.new
-    store.rentals_store(rental)
+    Store.rentals_store(rental)
     success('Rental')
     run
   end
 
   def list_rentals
     puts 'Enter a person id: '
-    store = Store.new
-    @people_arr = store.people_arr
-    @rentals_arr = store.rentals_arr
-    @people_arr.each { |person| puts "#{person.name} - Person ID: #{person.id}" }
+    Store.people_arr.each { |person| puts "#{person.name} - Person ID: #{person.id}" }
     person_id = gets.chomp.to_i
-    rentals = @rentals_arr.select { |rental| rental.person.id == person_id }
-
+    rentals = Store.rentals_arr.select { |rental| rental.person.id == person_id }
     if rentals.empty?
       puts "Person with ID #{person_id} has no rentals yet"
     else
